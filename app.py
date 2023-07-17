5dc5e13b08f68a01c31b75bc5b88174ae289b160
Armin Radoncic <Arminradoncic1@gmail.com>
2023-07-17 14:44:59 -0400

    Format App class for readability

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from argparse import ArgumentParser
 from password_vault import PasswordVault
 
+
 class App:
     def __init__(self):
         self.vault = PasswordVault()
@@ -14,37 +15,57 @@ class App:
         self.initialize_gen_cmd()
 
     def initialize_setup_cmd(self):
-        self.parser_setup_cmd = self.subparsers.add_parser('setup')
+        self.parser_setup_cmd = self.subparsers.add_parser("setup")
         self.parser_setup_cmd.set_defaults(func=self.vault.initialize_user)
 
     def initialize_set_cmd(self):
-        self.parser_set_cmd = self.subparsers.add_parser('set')
-        self.parser_set_cmd.add_argument("master_password", type=str, help="The master password made in setup")
-        self.parser_set_cmd.add_argument("service", type=str, help="The service you wish to pair the password with")
-        self.parser_set_cmd.add_argument("password", type=str, help="The password you want to set")
+        self.parser_set_cmd = self.subparsers.add_parser("set")
+        self.parser_set_cmd.add_argument(
+            "master_password", type=str, help="The master password made in setup"
+        )
+        self.parser_set_cmd.add_argument(
+            "service", type=str, help="The service you wish to pair the password with"
+        )
+        self.parser_set_cmd.add_argument(
+            "password", type=str, help="The password you want to set"
+        )
         self.parser_set_cmd.set_defaults(func=self.vault.write_password)
 
     def initialize_get_cmd(self):
-        self.parser_get_cmd = self.subparsers.add_parser('get')
-        self.parser_get_cmd.add_argument("master_password", type=str, help="The master password made in setup")
-        self.parser_get_cmd.add_argument("service", type=str, help="The service whose password you want to get")
+        self.parser_get_cmd = self.subparsers.add_parser("get")
+        self.parser_get_cmd.add_argument(
+            "master_password", type=str, help="The master password made in setup"
+        )
+        self.parser_get_cmd.add_argument(
+            "service", type=str, help="The service whose password you want to get"
+        )
         self.parser_get_cmd.set_defaults(func=self.vault.get_password)
 
     def initialize_del_cmd(self):
-        self.parser_del_cmd = self.subparsers.add_parser('del')
-        self.parser_del_cmd.add_argument("master_password", type=str, help="The master password made in setup")
-        self.parser_del_cmd.add_argument("service", type=str, help="The service you wish to delete")
+        self.parser_del_cmd = self.subparsers.add_parser("del")
+        self.parser_del_cmd.add_argument(
+            "master_password", type=str, help="The master password made in setup"
+        )
+        self.parser_del_cmd.add_argument(
+            "service", type=str, help="The service you wish to delete"
+        )
         self.parser_del_cmd.set_defaults(func=self.vault.del_password)
 
     def initialize_gen_cmd(self):
-        self.parser_gen_cmd = self.subparsers.add_parser('gen')
-        self.parser_gen_cmd.add_argument("master_password", type=str, help="The master password made in setup")
-        self.parser_gen_cmd.add_argument("service", type=str, help="The service you wish to pair the randomly generated password with")
+        self.parser_gen_cmd = self.subparsers.add_parser("gen")
+        self.parser_gen_cmd.add_argument(
+            "master_password", type=str, help="The master password made in setup"
+        )
+        self.parser_gen_cmd.add_argument(
+            "service",
+            type=str,
+            help="The service you wish to pair the randomly generated password with",
+        )
         self.parser_gen_cmd.set_defaults(func=self.vault.write_generated_password)
 
     def parse_arguments(self):
         args = self.parser.parse_args()
-        if hasattr(args, 'func'):
+        if hasattr(args, "func"):
             if args.func == self.vault.initialize_user:
                 args.func()
 
@@ -63,7 +84,7 @@ class App:
                 master_password = args.master_password
                 service = args.service
                 args.func(master_password, service)
-            
+
             elif args.func == self.vault.write_generated_password:
                 master_password = args.master_password
                 service = args.service
